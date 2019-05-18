39fa941452edea97178f0471f1ff6a4757fc9752
proton0001 <cs15btech11029@gmail.com>
2019-05-18 13:43:03 +0530

    Scripts + All checked files inseperate directory

---
+++ verifyFiles.sh
@@ -0,0 +1,15 @@
+#/bin/bash -x
+LOGFILE=log_`date +%Y-%m-%d-%H-%M`_verify
+
+rm -f $LOGFILE
+touch $LOGFILE
+
+for FILE in $(seq 1 $1); do
+  if [ -f "$FILE.f95" ]; then
+    echo "$FILE.f95" >> $LOGFILE
+    bash verifySingpleFile.sh $FILE.f95 2>&1 | tee -a $LOGFILE
+    echo "*******************" >> $LOGFILE
+  fi
+done
+
+
---
+++ verifySingpleFile.sh
@@ -0,0 +1,20 @@
+
+FILE=`basename $1 .f95`
+LLVM_BUILD=/home/cs15btech11029/OmpVerifier/build
+flang -fopenmp -S -emit-llvm  $FILE.f95 -o $FILE.ll
+if [ $? -ne 0 ]; then
+	echo "Falng failed for $FILE.f95"
+	exit
+fi
+$LLVM_BUILD/bin/opt  -mem2reg $FILE.ll -S -o $FILE.ssa.ll
+$LLVM_BUILD/bin/opt  -load $LLVM_BUILD/lib/OpenMPVerify.so  -openmp-resetbounds $FILE.ssa.ll -S -o $FILE.resetbounds.ll 
+$LLVM_BUILD/bin/opt  -load $LLVM_BUILD/lib/OpenMPVerify.so  -polly-process-unprofitable -polly-invariant-load-hoisting -polly-ignore-parameter-bounds --analyze -openmp-analysis $FILE.resetbounds.ll 
+$LLVM_BUILD/bin/opt  -load $LLVM_BUILD/lib/OpenMPVerify.so  -polly-process-unprofitable -polly-invariant-load-hoisting -polly-ignore-parameter-bounds --analyze -openmp-verify $FILE.resetbounds.ll
+
+
+
+
+#$LLVM_BUILD/bin/opt  -load $LLVM_BUILD/lib/OpenMPVerify.so  -polly-process-unprofitable -polly-invariant-load-hoisting --analyze -debug-only=openmp-analysis -openmp-analysis $FILE.resetbounds.ll
+#$LLVM_BUILD/bin/opt  -load $LLVM_BUILD/lib/OpenMPVerify.so  -polly-process-unprofitable -polly-invariant-load-hoisting  -polly-ignore-parameter-bounds ---analyze -debug-only=openmp-verify  -openmp-verify $FILE.resetbounds.ll
+
+
