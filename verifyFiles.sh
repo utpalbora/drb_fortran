59f27244f0483c68a002198c9d35512b54d4cf10
Utpal Bora <cs14mtech11017@iith.ac.in>
2019-06-10 19:11:37 +0530

    Generic path

--- verifyFiles.sh
+++ verifyFiles.sh
@@ -1,12 +1,12 @@
 #/bin/bash -x
 LOGFILE=`date +%Y-%m-%d-%H-%M`_verify.log
-ROOT_DIR=/home/cs15btech11029/OmpVerifier/drb_fortran
+ROOT_DIR=`pwd`
 rm -f $LOGFILE
 touch $LOGFILE
 
 for FILE in $(seq 1 $1); do
   if [ -f "$FILE.f95" ]; then
-    echo "$FILE.f95" >> $LOGFILE
+    echo "$FILE.f95" | tee -a $LOGFILE
     bash $ROOT_DIR/verifySingpleFile.sh $FILE.f95 2>&1 | tee -a $LOGFILE
     echo "*******************" >> $LOGFILE
   fi
