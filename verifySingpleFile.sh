d1fb5d9a44a1ffafe5483b0a9d47064b87a8c40c
Utpal Bora <cs14mtech11017@iith.ac.in>
2019-06-11 10:14:58 +0530

    Typo fix and remove redundant operation

--- verifySingpleFile.sh
+++ verifySingpleFile.sh
@@ -11,12 +11,10 @@ FLANG_PATH=/mnt/OldExtra/Utpal/Flang/install/bin
 $FLANG_PATH/flang -fopenmp -S -emit-llvm -O1 $FILE.f95 -o $FILE.ll
 
 if [ $? -ne 0 ]; then
-	echo "Falng failed for $FILE.f95"
+	echo "Flang failed for $FILE.f95"
 	exit
 fi
 
 $LLVM_BUILD/bin/opt  -mem2reg $FILE.ll -S -o $FILE.ssa.ll
 $LLVM_BUILD/bin/opt  -load $LLVM_BUILD/lib/OpenMPVerify.so  -openmp-resetbounds $FILE.ssa.ll -S -o $FILE.resetbounds.ll
-$FLANG_PATH/flang -fopenmp -S -emit-llvm  $FILE.f95 -o $FILE.ll
-#$LLVM_BUILD/bin/opt  -load $LLVM_BUILD/lib/OpenMPVerify.so -polly-detect-fortran-arrays -polly-process-unprofitable -polly-invariant-load-hoisting -polly-ignore-parameter-bounds --analyze -openmp-analysis -polly-dependences-on-demand $FILE.resetbounds.ll
 $LLVM_BUILD/bin/opt  -load $LLVM_BUILD/lib/OpenMPVerify.so -polly-detect-fortran-arrays -polly-process-unprofitable -polly-invariant-load-hoisting -polly-ignore-parameter-bounds -openmp-verify --disable-output -polly-dependences-on-demand $FILE.resetbounds.ll
