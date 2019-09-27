47d436ed3c386a946b6daf90eabc2c945068ec9f
Utpal Bora <cs14mtech11017@iith.ac.in>
2019-09-28 00:09:08 +0530

    XUpdated scripts. Kernel 26 is now compiling successfully by Flang.

--- scripts/verifyFiles.sh
+++ scripts/verifyFiles.sh
@@ -1,14 +1,20 @@
 #/bin/bash -x
-LOGFILE=`date +%Y-%m-%d-%H-%M`_verify.log
 ROOT_DIR=$(pwd)
+OUT_DIR=$ROOT_DIR/results
+LOG_DIR=$OUT_DIR/log
+LOGFILE=$LOG_DIR/`date +%Y-%m-%d-%H-%M`_verify.log
+
+mkdir -p $OUT_DIR
+mkdir -p $LOG_DIR
 
 rm -f $LOGFILE
 touch $LOGFILE
 
-for FILE in $(seq 1 $1); do
-  if [ -f "$FILE.f95" ]; then
-    echo "$FILE.f95" | tee -a $LOGFILE
-    bash $ROOT_DIR/verifySingpleFileO1.sh $FILE.f95 1 2>&1 | tee -a $LOGFILE
+for FILE in $(ls $ROOT_DIR/micro-benchmarks/*.f95); do
+#for FILE in $(ls $ROOT_DIR/FlangError/*.f95); do
+  if [ -f $FILE ]; then
+    echo $FILE | tee -a $LOGFILE
+    $ROOT_DIR/scripts/verifySingpleFile.sh $FILE 1 2>&1 | tee -a $LOGFILE
     echo "*******************" >> $LOGFILE
   fi
 done
--- scripts/verifySingpleFile.sh
+++ scripts/verifySingpleFile.sh
@@ -1,20 +1,43 @@
-#/bin/bash -x
+#!/bin/bash
+LLOV_BUILD=/home/utpal/Work/LLVMOMPVerify/build
+FLANG_PATH=/home/utpal/installs/flang-2019-09-04/bin
 
-FILE=`basename $1 .f95`
+TEST=$1
+CLEAR=$2
+FILENAME=$(basename $TEST)
+FILENAME=${FILENAME%.*}
+OUT_DIR="results"
+FILE=$OUT_DIR/$FILENAME
 
-#LLVM_BUILD=/home/utpal/LLVMOmpVerify/build
-#FLANG_PATH=/home/utpal/installs/flang-2019-05-18/bin
+LOG_DIR=$OUT_DIR/log
+LOGFILE=$LOG_DIR/$FILENAME.log
 
-LLVM_BUILD=/home/cs15btech11029/OmpVerifier/build
-FLANG_PATH=/mnt/OldExtra/Utpal/Flang/install/bin
+#mkdir -p $OUT_DIR
+#mkdir -p $LOG_DIR
 
-$FLANG_PATH/flang -fopenmp -S -emit-llvm  $FILE.f95 -o $FILE.ll
+function exitTest {
+  if [ $1 -ne 0 ]; then
+    echo "Flang failed for $TEST"
+    exit 1
+  fi
+}
 
-if [ $? -ne 0 ]; then
-	echo "Flang failed for $FILE.f95"
-	exit
+$FLANG_PATH/flang -fopenmp -S -emit-llvm "$TEST" -o "$FILE.ll"
+exitTest $?
+
+$LLOV_BUILD/bin/opt -mem2reg -O1 "$FILE.ll" -S -o "$FILE.ssa.ll" > /dev/null 2>&1
+exitTest $?
+$LLOV_BUILD/bin/opt -load $LLOV_BUILD/lib/OpenMPVerify.so \
+  -openmp-resetbounds "$FILE.ssa.ll" -S -o "$FILE.resetbounds.ll" > /dev/null 2>&1
+exitTest $?
+$LLOV_BUILD/bin/opt -load $LLOV_BUILD/lib/OpenMPVerify.so \
+  -polly-detect-fortran-arrays -polly-process-unprofitable \
+  -polly-invariant-load-hoisting -polly-ignore-parameter-bounds \
+  -polly-dependences-on-demand -disable-output \
+  -openmp-verify \
+  "$FILE.resetbounds.ll" > "$LOGFILE" 2>&1
+
+if [ ! -z $CLEAR ]; then
+  rm -rf "$FILE.resetbounds.ll" "$FILE.ssa.ll" "$FILE.ll"
 fi
 
-$LLVM_BUILD/bin/opt  -mem2reg $FILE.ll -S -o $FILE.ssa.ll
-$LLVM_BUILD/bin/opt  -load $LLVM_BUILD/lib/OpenMPVerify.so  -openmp-resetbounds $FILE.ssa.ll -S -o $FILE.resetbounds.ll
-$LLVM_BUILD/bin/opt  -load $LLVM_BUILD/lib/OpenMPVerify.so -polly-detect-fortran-arrays -polly-process-unprofitable -polly-invariant-load-hoisting -polly-ignore-parameter-bounds -openmp-verify --disable-output -polly-dependences-on-demand $FILE.resetbounds.ll
