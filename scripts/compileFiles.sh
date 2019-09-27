b3c37db1c719285e7602abce055eda86c6e3464d
Utpal Bora <cs14mtech11017@iith.ac.in>
2019-09-28 00:24:04 +0530

    Updated script to compile and test kernels

--- scripts/compileFiles.sh
+++ scripts/compileFiles.sh
@@ -1,13 +1,13 @@
 #/bin/bash -x
 
-for FILE in $(seq 1 $1); do
-  if [ -f "$FILE.f95" ]; then
-    out=$(gfortran $FILE.f95 -o $FILE.out 2>&1)
+for FILE in $(ls micro-benchmarks/*.f95); do
+  if [ -f "$FILE" ]; then
+    out=$(gfortran $FILE -o ${FILE%.*}.out 2>&1)
     if [ $? -ne 0 ]; then
-      printf "$FILE.f95 : Failed\n"
+      printf "$FILE : Failed\n"
     else
-      runOutput=$(./$FILE.out)
-      printf "$FILE.f95 : $runOutput\n"
+      runOutput=$(./${FILE%.*}.out)
+      printf "$FILE : $runOutput\n"
     fi
   fi
 done
