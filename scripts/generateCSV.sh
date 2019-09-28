9d1eef7732567aa3d2f62f165867c6bdb2fabe7e
Utpal Bora <cs14mtech11017@iith.ac.in>
2019-09-28 13:56:41 +0530

    Generate CSV.

--- scripts/generateCSV.sh
+++ scripts/generateCSV.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-echo "id, race, norace" > drb_fortran.csv
-for l in $(seq -w 001 116); do
-  LOG=results/log/DRBF${l}
-  race=$(grep -ce "Data Race detected" ${LOG}*.log);
-  free=$(grep -ce "Data Race Free" ${LOG}*.log);
-  echo "$l, $race, $free" >> drb_fortran.csv;
+echo "id, haverace, race, norace" > drb_fortran.csv
+#for l in $(seq -w 001 116); do
+for l in $(ls micro-benchmarks/*.f95); do
+  filename="${l%%.*}"
+  filename="${filename##*/}"
+  haverace="${filename##*_}"
+  id=${filename#DRBF}
+  id=${id%%_*}
+  LOG=results/log/${filename}.log
+  race=$(grep -ce "Data Race detected." ${LOG});
+  free=$(grep -ce "Data Race Free" ${LOG});
+  echo "$id, ${haverace}, ${race:-"NA"}, ${free:-"NA"}" >> drb_fortran.csv;
 done
